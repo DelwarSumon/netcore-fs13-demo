354580d054f0daf52aa351a19e92ac52be2de3e9
Phi Van Ngoc <ngocketit@gmail.com>
2023-03-07 18:05:29 +0200

    Add user signin endpoint

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,13 +3,16 @@ namespace NETCoreDemo.Controllers;
 using NETCoreDemo.Services;
 using NETCoreDemo.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
+[Authorize]
 public class UserController : ApiControllerBase
 {
     private readonly IUserService _service;
 
     public UserController(IUserService service) => _service = service;
 
+    [AllowAnonymous]
     [HttpPost("signup")]
     public async Task<IActionResult> SignUp(UserSignUpDTO request)
     {
@@ -20,4 +23,17 @@ public class UserController : ApiControllerBase
         }
         return Ok(UserSignUpResponseDTO.FromUser(user));
     }
+
+    [AllowAnonymous]
+    [HttpPost("signin")]
+    public async Task<IActionResult> SignIn(UserSignInDTO request)
+    {
+        var response = await _service.SignInAsync(request);
+        if (response is null)
+        {
+            return Unauthorized();
+        }
+        return Ok(response);
+    }
+    // TODO: Implement other endpoints for users
 }
