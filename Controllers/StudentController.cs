278e59992cb745b996a5c9000825f9d58c14f5dd
Phi Van Ngoc <ngocketit@gmail.com>
2023-03-07 18:05:49 +0200

    Add authorize policy to student controller

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -3,7 +3,9 @@ namespace NETCoreDemo.Controllers;
 using NETCoreDemo.Services;
 using NETCoreDemo.Models;
 using NETCoreDemo.DTOs;
+using Microsoft.AspNetCore.Authorization;
 
+[Authorize(Policy = "FS13")]
 public class StudentController : CrudController<Student, StudentDTO>
 {
     private readonly ILogger<StudentController> _logger;
